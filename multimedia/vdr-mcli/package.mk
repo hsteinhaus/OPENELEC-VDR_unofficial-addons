2313465dd1dc9b0845d36ac50013f64536b1429a
Holger Steinhaus <hsteinhaus@gmx.de>
2015-11-08 19:38:47 +0100

    vdr-mcli: receive IPv6 multicast DVB stream from Reelboxes/Netceivers

---
+++ multimedia/vdr-mcli/package.mk
@@ -0,0 +1,62 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2014 Stephan Raue (stephan@openelec.tv)
+#
+#  OpenELEC is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 2 of the License, or
+#  (at your option) any later version.
+#
+#  OpenELEC is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.  If not, see <http://www.gnu.org/licenses/>.
+################################################################################
+
+PKG_NAME="vdr-mcli"
+PKG_VERSION="0.9.0"
+PKG_REV="1"
+PKG_ARCH="any"
+PKG_LICENSE="GPL"
+PKG_SITE="http://projects.vdr-developer.org/projects/plg-mcli"
+PKG_URL="http://projects.vdr-developer.org/attachments/download/1973/$PKG_NAME-$PKG_VERSION.tar.gz"
+PKG_SOURCE_DIR="mcli-$PKG_VERSION"
+PKG_DEPENDS_TARGET="toolchain libxml2 vdr"
+PKG_PRIORITY="optional"
+PKG_SECTION="multimedia"
+PKG_SHORTDESC="vdr-plugin-mcli: Netceiver support for the Video Disk Recorder (VDR)"
+PKG_LONGDESC="vdr-plugin-mcli: Netceiver support for the Video Disk Recorder (VDR)"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+make_target() {
+  VDR_DIR=$(get_build_dir vdr)
+  export PKG_CONFIG_PATH=$VDR_DIR:$PKG_CONFIG_PATH
+  export CPLUS_INCLUDE_PATH=$VDR_DIR/include
+
+#  XML_INC=`$ROOT/$TOOLCHAIN/bin/xml2-config --cflags`
+#  XML_LIB=`$ROOT/$TOOLCHAIN/bin/xml2-config --libs`
+
+  make \
+    LOCALEDIR=./locale \
+    VDRDIR=$VDR_DIR \
+    LIBDIR="." \
+    plugin i18n
+}
+
+post_make_target() {
+  VDR_DIR=$(get_build_dir vdr)
+  VDR_APIVERSION=`sed -ne '/define APIVERSION/s/^.*"\(.*\)".*$/\1/p' $VDR_DIR/config.h`
+  LIB_NAME=lib${PKG_NAME/-plugin/}
+
+  cp --remove-destination $ROOT/$PKG_BUILD/${LIB_NAME}.so $ROOT/$PKG_BUILD/${LIB_NAME}.so.${VDR_APIVERSION}
+  $STRIP libvdr-*.so*
+}
+
+makeinstall_target() {
+  : # installation not needed, done by create-addon script
+}
