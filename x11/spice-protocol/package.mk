58e00b37618b805d3f1202efad1b85e46a21ae41
Holger Steinhaus <hsteinhaus@gmx.de>
2016-02-25 19:21:19 +0100

    spice-protocol: a remote desktop protocol

---
+++ x11/spice-protocol/package.mk
@@ -0,0 +1,37 @@
+################################################################################
+#      This file is part of OpenELEC - http://www.openelec.tv
+#      Copyright (C) 2009-2012 Stephan Raue (stephan@openelec.tv)
+#
+#  This Program is free software; you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation; either version 2, or (at your option)
+#  any later version.
+#
+#  This Program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#  GNU General Public License for more details.
+#
+#  You should have received a copy of the GNU General Public License
+#  along with OpenELEC.tv; see the file COPYING.  If not, write to
+#  the Free Software Foundation, 51 Franklin Street, Suite 500, Boston, MA 02110, USA.
+#  http://www.gnu.org/copyleft/gpl.html
+################################################################################
+
+PKG_NAME="spice-protocol"
+PKG_VERSION="0.12.10"
+PKG_REV="1"
+PKG_ARCH=""
+PKG_LICENSE="GPL"
+PKG_SITE="http://www.spice-space.org"
+PKG_URL="${PKG_SITE}/download/releases/${PKG_NAME}-${PKG_VERSION}.tar.bz2"
+PKG_DEPENDS_TARGET="toolchain x11 cairo gtk+ libXinerama"
+PKG_PRIORITY="optional"
+PKG_SECTION="x11/app"
+PKG_SHORTDESC="The SPICE protocol"
+PKG_LONGDESC="The SPICE protocol"
+
+PKG_IS_ADDON="no"
+PKG_AUTORECONF="no"
+
+PKG_CONFIGURE_OPTS_TARGET="--prefix=/usr"
